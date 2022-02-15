c5ac88ec2357462fd66076ea334d3fb4cfdc6bcd
Sushil Ghate <sushilsg30@gmail.com>
2022-02-15 17:55:25 +0530

    block-BJaaay

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,33 +4,33 @@
 
 // Using `console.log` log all the values from 1 to 10.
 for (let i =0; i < 10; i = i + 1 ); {
-    console.log (i);
+    console.log (`i`);
 }
 
 // Using `console.log` log all the evan values from 1 to 10.
 for (let i =0; i < 10; i = i + 1 ); {
-    if (i % 2 == 0 );{
-    console.log (i);
+    if (i % 2 === 0 );{
+    console.log (i `even`);
     }
 }
 
 // Using `console.log` log all the od values from 1 to 10.
 for (let i =0; i < 10; i = i + 1 ); {
     if (i % 2 !== 0 );{
-    console.log (i);
+    console.log (i `odd`);
     }
 }
 
 // Calculate the sum of all numbers from 1 to 10.
-for (let i =0; i < 10; i = i + 1 ); {
-    if (i = i + (i+1) + (i+2) + (i+3) + (i+4) + (i+5) (i+6) + (i+7) + (i+8) + (i+9));{
-    console.log (i);
-    }
+let sum = 0;
+for (let i =1; i <= 10; i++) {
+    sum +=i;
 }
 
+
 // Log all the values from 1 to 10 using while loop
-let i = i +1;
+let i = 1;
 while (i < 10) {
     console.log(i);
-    i = i + 1;
+    i++;
 }
